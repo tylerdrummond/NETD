7d58ef202fe195ce7e95c4946a864c2ef30ee52e
tylerdrummond <35852327+tylerdrummond@users.noreply.github.com>
2018-02-02 16:50:18 -0500

    Add files via upload

---
+++ AverageUnitsShipped/AverageUnitsShipped/AverageUnitsShipped.vb
@@ -0,0 +1,14 @@
+﻿''' <summary>
+''' Author: Tyler Drummond
+''' Project: Average Units Shipped
+''' Date: 02-02-2018
+''' Description: This windows application will take user input as an integer and store it into 
+'''              an array, after seven inputs, the program will calculate the average and display 
+'''              it appropriatly.
+''' </summary>
+
+Public Class FrmAvgUnitsShipped
+
+
+End Class
+
